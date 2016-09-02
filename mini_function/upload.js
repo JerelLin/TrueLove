7ec032b73c761e32b2a60694745b2a44e5862c66
JerelLin <13531957084@163.com>
2016-09-02 10:28:14 +0800

    修正发布活动时上传图片后缀名不显示的问题

--- mini_function/upload.js
+++ mini_function/upload.js
@@ -9,13 +9,12 @@ function upload( req, resolve, reject ){
 	// 图片文件将要上传到哪个文件夹下面
 	let uploadfoldername = "uploadfiles";
 	let uploadfolderpath = path.resolve( __dirname, "../asset/", uploadfoldername );
-	console.log( uploadfolderpath )
 
 	let form = new formidable.IncomingForm({
 		encoding : "utf-8",
 	        	uploadDir : uploadfolderpath,
 	        	keepExtensions : true
-	});
+	})
 
 	let fieldList = {
 
@@ -24,11 +23,29 @@ function upload( req, resolve, reject ){
 
 	form.parse( req )
 		.on( "field", function( name, value ) { 
-			fieldList[ name ] = value;
+			fieldList[ name ] = value
 		} )
 		.on( "file", function( name, file ) {
-			let filename = path.basename( file.path );
-			let filepath = "http://" + server + ":" + port + "/" + uploadfoldername + "/" + filename;
+			let filename = ( new Date(  ) ).getTime(  );
+		            switch ( file.type ){
+		            	case "image/jpeg" :
+		                    		filename = filename + ".jpg"
+		                    		break
+		                	case "image/png" :
+		                    		filename = filename + ".png"
+		                    		break
+		                	default :
+		                    		filename = filename + ".png"
+		                   		break
+		            }
+		     	let file_save_path = uploadfolderpath + "/" + filename
+		     	console.log( file.path )
+		            fs.rename( file.path, file_save_path, function(err) {
+		            	if ( err ) {
+		            		console.log( err )
+		                	}
+		            } )
+		            let filepath = "http://" + server + ":" + port + "/" + uploadfoldername + "/" + filename
 			fileList.push( filepath )
 		} )
 		.on( "end", function(  ){
